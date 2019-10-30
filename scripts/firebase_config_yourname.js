9edcd7d578e11f2447a1366640d6ee18a8c813c9
Carly Orr <carly_wong_orr@bcit.ca>
2019-10-30 10:03:44 -0700

    fixed footer and firebase config file

--- scripts/firebase_config_yourname.js
+++ scripts/firebase_config_yourname.js
@@ -1,16 +1,15 @@
-   // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyCTjQNdAlqs7OeETdTvePiBTkkKCLfwP44",
-      authDomain: "fir-eb9ef.firebaseapp.com",
-      databaseURL: "https://fir-eb9ef.firebaseio.com",
-      projectId: "fir-eb9ef",
-      storageBucket: "fir-eb9ef.appspot.com",
-      messagingSenderId: "922951246749",
-      appId: "1:922951246749:web:9a0cc62c6d2be171ef4574",
-      measurementId: "G-6NPCQH1NVP"
-    };
-    // Initialize Firebase
-    // Henceforce, any reference to the database can be made with "db"
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.firestore();
-    //db.settings({timestampsInSnapshots: true});
+//---------------------------------------------------------------------
+// Your web app's Firebase configuration;
+// Speficies which firebase project your application is connected with.
+//---------------------------------------------------------------------
+
+var firebaseConfig = {
+    
+  // Your API stuff goes here;  get it from firebase console
+  
+};
+
+// Initialize Firebase
+// Henceforce, any reference to the database can be made with "db"
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
